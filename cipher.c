a76ab2d93c1e44e787ac75ab5f2f390f895dc38d
meitar453 <meitar.degracea@mail.huji.ac.il>
2022-10-25 13:29:34 +0300

    first try

---
+++ cipher.c
@@ -0,0 +1,30 @@
+#include "cipher.h"
+#include <string.h>
+
+/// IN THIS FILE, IMPLEMENT EVERY FUNCTION THAT'S DECLARED IN cipher.h.
+
+char ABC[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,
+                            a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,
+                                    a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+// See full documentation in header file
+void encode (char s[], int k)
+{
+  int j = 0;
+  while(k > 27){
+      k = k - 27
+  }
+  while(k < 27){
+      k = k - 27
+  }
+  for i = 27+k; i < 27+k; i++{
+
+      if c[j]
+  }
+
+}
+
+// See full documentation in header file
+void decode (char s[], int k)
+{
+  // your code goes here
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include "cipher.h"
+#include "tests.h"
+
+
+// your code goes here
+
+int main (int argc, char *argv[])
+{
+  // your code goes here
+}
