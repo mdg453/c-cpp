f9576b4865ea92b8db8b80d12eddd0501199b6c1
meitar453 <meitar.degracea@mail.huji.ac.il>
2022-11-03 15:38:58 +0200

    try 6

--- main.c
+++ main.c
@@ -10,21 +10,21 @@
 int check_list (int argc, char *argv[])
 {
     if(argc != ARGV2 && argc != ARGV5){
-        printf("The program receives 1 or 4 arguments only.\n" ) ;
+        fprintf(stderr,"The program receives 1 or 4 arguments only.\n" ) ;
         return 0 ;
     }
     if (argc == ARGV2 && !strcmp(argv[1], "test"))
     {
-        printf("Usage: cipher test\n" ) ;
+        fprintf(stderr,"Usage: cipher test\n" ) ;
         return 0 ;
     }
     if ((strcmp(argv[1], "encode") != 0 ) && (strcmp(argv[1] , "decode") != 0))
     {
-    printf("The given command is invalid.\n");
+        fprintf(stderr,"The given command is invalid.\n");
         return EXIT_FAILURE ;
     }
     if(*argv[4]%1 != 0){
-        printf("The given shift value is invalid.\n" ) ;
+        fprintf(stderr,"The given shift value is invalid.\n" ) ;
         return 0 ;
     }
     return 1 ;
@@ -38,10 +38,10 @@ int tester()
             test_encode_cyclic_lower_case_negative_k() &&
                 test_encode_cyclic_upper_case_positive_k() &&
                     test_decode_non_cyclic_lower_case_positive_k() &&
-                        test_decode_cyclic_lower_case_special_char_positive_k() &&
-                            test_decode_non_cyclic_lower_case_special_char_negative_k() &&
-                                test_decode_cyclic_lower_case_negative_k() &&
-                                    test_decode_cyclic_upper_case_positive_k())
+        test_decode_cyclic_lower_case_special_char_positive_k() &&
+            test_decode_non_cyclic_lower_case_special_char_negative_k() &&
+                test_decode_cyclic_lower_case_negative_k() &&
+                    test_decode_cyclic_upper_case_positive_k())
     {
         return 1 ;
     }
@@ -64,13 +64,13 @@ int main (int argc, char *argv[])
     FILE *in = fopen (input_path, "r") ;
     if (in == NULL)
     {
-        printf("\nThe given file is invalid.\n" ) ;
+        fprintf(stderr,"\nThe given file is invalid.\n" ) ;
         return EXIT_FAILURE ;
     }
     FILE *out = fopen(output_path, "w") ;
     if (out == NULL)
     {
-        printf("\nThe given file is invalid.\n" ) ;
+        fprintf(stderr,"\nThe given file is invalid.\n" ) ;
         return EXIT_FAILURE ;
     }
     if(strcmp(argv[1] , "encode"))
